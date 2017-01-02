74ef448a867f5f8394ed29d233b26d887477404d
James Horton <valeness84@gmail.com>
2017-01-02 01:24:29 -0700

    Added Register Function and Validation

--- app/Http/Controllers/WoWController.php
+++ app/Http/Controllers/WoWController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use DB;
 use Request;
+use Validator;
 use View;
 
 class WoWController extends Controller
@@ -14,13 +15,38 @@ class WoWController extends Controller
     }
 
     public function index() {
-        $conn = DB::connection('mysql');
         return View::make('wow');
     }
 
     public function register() {
         $req = Request::all();
-        return $req;
+        $retval = ['status' => 1, 'errors' => []];
+        $validator = Validator::make($req, [
+            'username' => 'required|min:3|max:50',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            $retval['status'] = 0;
+            $retval['errors'] = $validator->errors();
+        } else {
+//            try {
+                $conn = DB::connection('mysql');
+                $exist_check = $conn->select('SELECT * FROM account WHERE lower(trim(username)) like ?', [strtolower(trim($req['username']))]);
+                if(!empty($exist_check)) {
+                    $retval['status'] = 0;
+                    $retval['errors'][] = 'An account with that username already exists!';
+                } else {
+                    $conn->insert('INSERT INTO account (username, sha_pass_hash) VALUES (:username, :password)', ['username' => strtoupper($req['username']), 'password' => sha1(strtoupper($req['username'] . ':' . strtoupper($req['password'])))]);
+                }
+//            } catch(\Exception $e) {
+//                $retval['errors'][] = 'A Database Error Occurred';
+//                $retval['status'] = 0;
+//            }
+        }
+
+        return $retval;
+
     }
 
 }
